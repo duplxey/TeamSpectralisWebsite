70a87067a19e2003162fb83ba4a76f490dd3774b
duplxey <nick.tomazic@gmail.com>
2019-06-19 20:44:10 +0200

    Improved navbar

--- components/footer.php
+++ components/footer.php
@@ -6,18 +6,26 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 col-md-4">
-                    Social
+                    <div class="footer-title">Contact</div>
                     <ul>
-                        <li><a href="#">Facebook</a></li>
-                        <li><a href="#">Twitter</a></li>
-                        <li><a href="#">Twitch</a></li>
+                        <li><i class="fas fa-envelope"></i> Email: <a href="#">support@spectralis.com</a></li>
+                        <li><i class="fas fa-keyboard"></i> Contact form: <a href="#">click here</a></li>
                     </ul>
                 </div>
                 <div class="col-12 col-md-4">
-                    B
+                    <div class="footer-title">Stay in touch</div>
+                    <div class="social">
+                        <a href="#"><i class="fab fa-facebook fa-lg social-icon"></i></a>
+                        <a href="#"><i class="fab fa-instagram fa-lg social-icon"></i></a>
+                        <a href="#"><i class="fab fa-twitter fa-lg social-icon"></i></a>
+                    </div>
                 </div>
                 <div class="col-12 col-md-4">
-                    C
+                    <div class="footer-title">About site</div>
+                    <ul>
+                        <li><a href="#">Terms & conditions</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
--- components/header.php
+++ components/header.php
@@ -2,21 +2,21 @@
     <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="#">
-                <img src="img/logo.png" width="32" height="32" alt="">
+                <img src="img/logo.png" width="36" height="36" alt="">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#toggler" aria-controls="toggler" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <div class="collapse navbar-collapse" id="toggler">
+                <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Team</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Media</a>
+                        <a class="nav-link" href="#"><span class="pulse">● </span>Live</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Contact</a>

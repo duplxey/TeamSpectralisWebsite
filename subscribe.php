37edc429eb53d1562a7cc0ae699df26857198af2
duplxey <nick.tomazic@gmail.com>
2019-06-20 12:38:39 +0200

    Added email subscription, partners site

--- subscribe.php
+++ subscribe.php
@@ -3,7 +3,7 @@
 <html lang="en">
     <head>
         <?php require_once "php/preimports.php" ?>
-        <title>Team Spectralis | Contact</title>
+        <title>Team Spectralis | Subscribe</title>
         <?php require_once "php/imports.php" ?>
     </head>
     <body>
@@ -16,39 +16,30 @@
         </div>
         <div class="section">
             <div class="container">
-                <section id="contact">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <form action="https://formspree.io/teamspectralis@mailinator.com" method="POST" class="contact-form">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <!-- Begin Mailchimp Signup Form -->
+                        <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+                        <style type="text/css">#mc-embedded-subscribe-form.validate{margin:0;padding:0;}</style>
+                        <div id="mc_embed_signup">
+                            <form action="https://gmail.us3.list-manage.com/subscribe/post?u=ff938294288b301bace654f32&amp;id=6a4cd1e8f2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" tcute catarget="_blank" novalidate>
+                                <div id="mc_embed_signup_scroll">
                                     <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="Your Name" name="name">
+                                        <input type="email" value="" name="EMAIL" class="form-control" id="mce-EMAIL" placeholder="Email address" required>
                                     </div>
-                                    <div class="form-group">
-                                        <input type="email" class="form-control" placeholder="Your Email" name="name">
-                                    </div>
-                                    <div class="form-group">
-                                        <textarea class="form-control" rows="4" placeholder="Your Message" name="name"></textarea>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Send</button>
-                                </form>
-                            </div>
-                            <div class="col-md-6">
-                                <h3>Other contact options</h3>
-                                <ul>
-                                    <li>Email: <a href="mailto:info@teamspectralis.com">info@teamspectralis.com</a></li>
-                                    <li>Discord: <a href="#">Spectralis Discord</a></li>
-                                </ul>
-                                <h3>Follow us on social media</h3>
-                                <div class="social">
-                                    <a href="#"><i class="fab fa-facebook fa-lg icon-link"></i></a>
-                                    <a href="#"><i class="fab fa-instagram fa-lg icon-link"></i></a>
-                                    <a href="#"><i class="fab fa-twitter fa-lg icon-link"></i></a>
+                                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ff938294288b301bace654f32_6a4cd1e8f2" tabindex="-1" value=""></div>
+                                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary" style="width:100%;"></div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
+                        <!--End mc_embed_signup-->
                     </div>
-                </section>
+                    <div class="col-12 col-md-6">
+                        <p>"We promise we won't spam you, but we might send you some cute cat pictures occasionally."</p>
+                        <img src="img/cat.jpg" class="img-fluid rounded" alt="Kitty" style="width: 150px;">
+                    </div>
+                </div>
             </div>
         </div>
         <?php include "components/footer.php" ?>

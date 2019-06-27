5c1c4b14bc2def0fc0acece9ee44318755c37a7a
duplxey <nick.tomazic@gmail.com>
2019-06-27 20:11:46 +0200

    Added 404

---
+++ error404.php
@@ -0,0 +1,20 @@
+<?php require_once 'php/config.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php require_once "php/preimports.php" ?>
+        <title>Team Spectralis | Error 404</title>
+        <?php require_once "php/imports.php" ?>
+    </head>
+    <body>
+        <?php include "components/header.php" ?>
+        <div class="section" style="background:url('img/cover/cover1.png')center;background-size:cover;">
+            <div class="container">
+                <h2>Error 404</h2>
+                <p>Our ferret cannot find a site with that URL. Are you sure you have the correct URL?</p>
+                <img src="img/error404.jpg" alt="Ferret searching" width="400">
+            </div>
+        </div>
+        <?php include "components/footer.php" ?>
+    </body>
+</html>

70ce2cac8d841cee07b3c77e9fb2ec8a16e40c46
duplxey <nick.tomazic@gmail.com>
2019-06-27 14:17:46 +0200

    Live twitch toggler

--- live.php
+++ live.php
@@ -11,18 +11,45 @@
         <div class="section" style="background:url('img/cover/cover1.png')center;background-size:cover;">
             <div class="container">
                 <h2>Live</h2>
-                <p>Our members are highly motivated to bring you the best live content they can provide.
-Check them out on Youtube or Twitch on their respective channels.</p>
+                <p>Our members are highly motivated to bring you the best live content they can provide. Check them out on Youtube or Twitch on their respective channels.</p>
             </div>
         </div>
         <div class="section">
             <div class="container">
-                <!-- TODO: list channels that are online, fullscreen one. -->
-                <iframe
-                        src="https://player.twitch.tv/?channel=ogswagter"
-                        class="livestream"
-                        allowfullscreen="true">
-                </iframe>
+                <div id="accordion">
+                    <div class="card">
+                        <div class="card-header" id="heading1">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="collapse1">Swagter</button>
+                            </h5>
+                        </div>
+                        <div id="collapse1" class="collapse show" aria-labelledby="heading1" data-parent="#accordion">
+                            <div class="card-body">
+                                <iframe
+                                        src="https://player.twitch.tv/?channel=ogswagter"
+                                        class="livestream"
+                                        allowfullscreen="true">
+                                </iframe>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="heading2">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link" data-toggle="collapse" data-target="#collapse2" aria-expanded="true" aria-controls="collapse2">Swagter</button>
+                            </h5>
+                        </div>
+                        <div id="collapse2" class="collapse" aria-labelledby="heading2" data-parent="#accordion">
+                            <div class="card-body">
+                                <iframe
+                                        src="https://player.twitch.tv/?channel=ogswagter"
+                                        class="livestream"
+                                        allowfullscreen="true">
+                                </iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <?php include "components/footer.php" ?>

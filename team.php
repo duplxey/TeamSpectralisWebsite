264212339e256020a431bf2210345e13d2647336
swagter <cimperman.jorg@gmail.com>
2019-10-13 00:12:31 +0200

    Updated team.

--- team.php
+++ team.php
@@ -77,16 +77,17 @@
                             <h4>Medo</h4>
                             <div class="social">
                                 <a href="https://www.youtube.com/channel/UCzz0jEKPFAet_qaglwqbXMQ/videos?view_as=subscriber"><i class="fab fa-youtube fa-lg icon-link"></i></a>
-                                <a href="https://www.twitch.tv/medoxs"><i class="fab fa-twitch fa-lg icon-link"></i></a>
+                                <a href="https://www.twitter.tv/medoxs"><i class="fab fa-twitch fa-lg icon-link"></i></a>
                             </div>
                         </div>
                     </div>
                     <div class="col-12 col-md-3 padded-top">
                         <div class="center">
-                            <img src="img/team/rexaw.jpg" class="avatar" alt="">
-                            <h4>Rexaw</h4>
+                            <img src="img/team/klevz.png" class="avatar" alt="">
+                            <h4>Klevz</h4>
                             <div class="social">
-                                <a href="https://twitter.com/R3XAW"><i class="fab fa-twitter fa-lg icon-link"></i></a>
+                                <a href="https://www.youtube.com/teamspectralis"><i class="fab fa-youtube fa-lg icon-link"></i></a>
+                                <a href="https://twitter.com/jost_klevze"><i class="fab fa-twitter fa-lg icon-link"></i></a>
                             </div>
                         </div>
                     </div>
@@ -142,26 +143,6 @@
                 </div>
                 <h3 class="padded-top">Competitive</h3>
                 <div class="row">
-                    <div class="col-12 col-md-3 padded-top">
-                        <div class="center">
-                            <img src="img/team/Smooth.png" class="avatar" alt="">
-                            <h4>Smooth</h4>
-                            <div class="social">
-                                <a href="https://twitter.com/SmoothYT2"><i class="fab fa-twitter fa-lg icon-link"></i></a>
-                                <a href="https://www.youtube.com/channel/UC8iHkiX9CrjBdrx0BT7PeiA?view_as=subscriber"><i class="fab fa-youtube fa-lg icon-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-3 padded-top">
-                        <div class="center">
-                            <img src="img/team/mEGNAROz.jpg" class="avatar" alt="">
-                            <h4>Megnaroz</h4>
-                            <div class="social">
-                                <a href="https://www.twitch.tv/megnaroz_"><i class="fab fa-twitch fa-lg icon-link"></i></a>
-                                <a href="https://www.youtube.com/channel/UCErnLqW9mLhqz4zGvtfKtGg"><i class="fab fa-youtube fa-lg icon-link"></i></a>
-                            </div>
-                        </div>
-                    </div>
                     <div class="col-12 col-md-3 padded-top">
                         <div class="center">
                             <img src="img/team/dinosaur.png" class="avatar" alt="">

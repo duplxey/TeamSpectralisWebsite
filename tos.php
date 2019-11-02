aef6ce9e316f897e93b8df465ee67fe9115221d4
swagter <cimperman.jorg@gmail.com>
2019-11-02 20:09:51 +0100

    updated link color a:active in tos

--- tos.php
+++ tos.php
@@ -1,5 +1,5 @@
 <?php
-
+echo '<style>a:active{color:#34ebe8;}</style></br>';
 echo '<span>NOT available yet,<span></br>';
 echo '<span>currently in the making...<span></br>';
 echo '<a href="https://teamspectralis.com/" style="text-decoration:none;"><p>&larr; Go back<p></a>';

6792f1204e235fda3c157133808202e6d8fb6dd1
swagter <cimperman.jorg@gmail.com>
2019-12-14 21:23:17 +0100

    added donation page.

---
+++ support.php
@@ -0,0 +1,35 @@
+<?php require_once 'php/config.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php require_once "php/preimports.php" ?>
+        <title>Team Spectralis | Support</title>
+        <meta name="description" content="Subscribe to get notified about everything new in Team Spectralis."/>
+        <meta name="keywords" content="sub, subscribe, mail, newsletter, news"/>
+        <meta property="og:title" content="Team Spectralis | Subscribe"/>
+        <meta property="og:description" content="Subscribe to get notified about everything new in Team Spectralis."/>
+        <meta property="og:image" content="https://teamspectralis.com/img/thumbnail.png"/>
+        <?php require_once "php/imports.php" ?>
+    </head>
+    <body>
+        <?php include "components/header.php" ?>
+        <div class="section" style="background:url('img/cover/cover2.png')center;background-size: cover;">
+            <div class="container">
+                <h2>Support us by donating</h2>
+                <p>We never expect donations, but if someone decides to donate, we highly appreciate it. Donations are only used for organization growth and upgrades. <i><small>(Member peripheral upgrades, merch funding, event hosting etc.)</small></i></p>
+            </div>
+        </div>
+        <div class="section">
+            <div class="container">
+                <div class="row">
+                    
+                       Want to support our organization? Tap on the following button to donate:                
+            </div>
+            <div class="row">
+            <a href="https://paypal.me/teamspectralis"><img src="img/subscribe/donate.png" class="img-fluid rounded" alt="donate" style="width: 100px;"></a>
+                </div>
+                </div>
+            </div>
+        <?php include "components/footer.php" ?>
+    </body>
+</html>

5d3644fd8b01cf8a4c94be040a18727c320ea8f8
duplxey <nick.tomazic@gmail.com>
2019-12-29 15:41:42 +0100

    Website now connects to the DB

--- php/config.php
+++ php/config.php
@@ -13,3 +13,15 @@ DEFINE("INSTAGRAM", "https://www.instagram.com/teamspectralis/");
 DEFINE("TWITTER", "https://twitter.com/TeamSpectralis");
 DEFINE("YOUTUBE", "https://www.youtube.com/c/TeamSpectralis");
 DEFINE("DISCORD", "https://invite.gg/teamspectralis");
+
+// MySQL credentials
+DEFINE("DB_USER", "HIDDEN");
+DEFINE("DB_PASSWORD", "HIDDEN");
+DEFINE("DB_HOST", "HIDDEN");
+DEFINE("DB_NAME", "HIDDEN");
+
+try {
+    $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
+} catch (PDOException $e) {
+    die("ERROR: Failed to connect to the MySQL database.");
+}

2832a174fdd4fe44fb295cd20c7fd0b320198f96
duplxey <nick.tomazic@gmail.com>
2019-12-29 18:39:25 +0100

    Added Apex, CS:GO settings display

--- member.php
+++ member.php
@@ -61,22 +61,22 @@ $result = $stmt->fetch();
                                 if ($result["youtube"] != null) echo '<a href="' . $result["youtube"] . '" class="social"><i class="fab fa-youtube fa-lg icon-link"></i></a>';
                                 if ($result["facebook"] != null) echo '<a href="' . $result["facebook"] . '" class="social"><i class="fab fa-facebook fa-lg icon-link"></i></a>';
                                 if ($result["instagram"] != null) echo '<a href="' . $result["instagram"] . '" class="social"><i class="fab fa-instagram fa-lg icon-link"></i></a>';
-                                if ($result["website"] != null) echo '<a href="' . $result["website"] . '" class="social"><i class="fab fab fa-safari fa-lg icon-link"></i></a>';
+                                if ($result["website"] != null) echo '<a href="' . $result["website"] . '" class="social"><i class="fab fab fas fa-globe fa-lg icon-link"></i></a>';
                                 if ($result["twitch"] != null) echo '<a href="' . $result["twitch"] . '" class="social"><i class="fab fa-twitch fa-lg icon-link"></i></a>';
                             ?>
                         </div>
                         <h4>About me</h4>
-                        <p><?= $result["description"] ?></p>
+                        <p class="justify"><?= $result["description"] ?></p>
                     </div>
                     <div class="col-12 col-md-6">
                         <?php
-                            $f_settings_sql = "SELECT * FROM `fortnite_settings` WHERE `id` = ?;";
+                            $f_settings_sql = "SELECT * FROM `member_fortnite_settings` WHERE `id` = ?;";
                             $f_settings_stmt = $pdo->prepare($f_settings_sql);
                             $f_settings_stmt->execute([$result["id"]]);
                             $f_settings_result = $f_settings_stmt->fetch();
 
                             if ($f_settings_stmt->rowCount() != 0) {
-                                echo "<h4>Settings</h4>";
+                                echo "<h4>Fortnite Settings</h4>";
                                 echo "<table class='table table-dark'>";
                                 echo "<tr><td>Sensitivity</td><td>" . $f_settings_result["sensitivity"] . "</td></tr>";
                                 echo "<tr><td>DPI</td><td>" . $f_settings_result["dpi"] . "</td></tr>";
@@ -88,6 +88,48 @@ $result = $stmt->fetch();
                                 echo "<tr><td>Edit</td><td>" . $f_settings_result["edit"] . "</td></tr>";
                                 echo "<tr><td>Use</td><td>" . $f_settings_result["use"] . "</td></tr>";
                                 echo "</table>";
+                            }
+
+                            $a_settings_sql = "SELECT * FROM `member_apex_settings` WHERE `id` = ?;";
+                            $a_settings_stmt = $pdo->prepare($a_settings_sql);
+                            $a_settings_stmt->execute([$result["id"]]);
+                            $a_settings_result = $a_settings_stmt->fetch();
+
+                            if ($a_settings_stmt->rowCount() != 0) {
+                                echo "<h4>Apex Settings</h4>";
+                                echo "<table class='table table-dark'>";
+                                echo "<tr><td>Sensitivity</td><td>" . $a_settings_result["sensitivity"] . "</td></tr>";
+                                echo "<tr><td>DPI</td><td>" . $a_settings_result["dpi"] . "</td></tr>";
+                                echo "<tr><td>Jump</td><td>" . $a_settings_result["jump"] . "</td></tr>";
+                                echo "<tr><td>Crouch</td><td>" . $a_settings_result["crouch"] . "</td></tr>";
+                                echo "<tr><td>Sprint</td><td>" . $a_settings_result["sprint"] . "</td></tr>";
+                                echo "<tr><td>Use</td><td>" . $a_settings_result["use"] . "</td></tr>";
+                                echo "</table>";
+                            }
+
+                            $c_settings_sql = "SELECT * FROM `member_cs_settings` WHERE `id` = ?;";
+                            $c_settings_stmt = $pdo->prepare($c_settings_sql);
+                            $c_settings_stmt->execute([$result["id"]]);
+                            $c_settings_result = $c_settings_stmt->fetch();
+
+                            if ($c_settings_stmt->rowCount() != 0) {
+                                echo "<h4>CS:GO Settings</h4>";
+                                echo "<table class='table table-dark'>";
+                                echo "<tr><td>Sensitivity</td><td>" . $c_settings_result["sensitivity"] . "</td></tr>";
+                                echo "<tr><td>DPI</td><td>" . $c_settings_result["dpi"] . "</td></tr>";
+                                echo "<tr><td>Zoom sensitivity</td><td>" . $c_settings_result["zoom_sensitivity"] . "</td></tr>";
+                                echo "<tr><td>Resolution</td><td>" . $c_settings_result["resolution"] . "</td></tr>";
+                                echo "<tr><td>Aspect ratio</td><td>" . $c_settings_result["aspect"] . "</td></tr>";
+                                echo "<tr><td>Scaling</td><td>" . $c_settings_result["scaling"] . "</td></tr>";
+                                echo "<tr><td>Crosshair</td><td>" . $c_settings_result["crosshair"] . "</td></tr>";
+                                echo "<tr><td>Viewmodel</td><td>" . $c_settings_result["viewmodel"] . "</td></tr>";
+                                echo "<tr><td>cl_bob</td><td>" . $c_settings_result["cl_bob"] . "</td></tr>";
+                                echo "</table>";
+                            }
+
+                            if ($f_settings_stmt->rowCount() == 0 && $a_settings_stmt->rowCount() == 0 && $c_settings_stmt->rowCount() == 0) {
+                                echo "<h4>Settings</h4>";
+                                echo "<p>This user does not have any settings displayed.</p>";
                             }
                         ?>
                     </div>

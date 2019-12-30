bf89c28a75bc02a8d2bbbae97d5ecabdfa6ca05e
swagter <cimperman.jorg@gmail.com>
2019-12-30 01:22:33 +0100

    updated Gtehnic live link.

--- live.php
+++ live.php
@@ -25,7 +25,7 @@
                     <?php
                     $YOUTUBE_CHANNELS = array(
                         "Team Spectralis" => "https://www.youtube.com/embed/live_stream?channel=UCYeVnOpF0Yt6ipAOJaFpz2g",
-                        "Gtehnic" => "https://www.youtube.com/embed/live_stream?channel=UCfBkq9Ndho8W2FJymDXeviw",
+                        "Gtehnic" => "https://www.youtube.com/embed/live_stream?channel=UChB4J-Y_qdtbucbdlLPIgDQ",
                         "ScorpLZ" => "https://www.youtube.com/embed/live_stream?channel=UCDJ_eyz-sgsnk6zvUiPlBDw",
                         "Clumsy Mark" => "https://www.youtube.com/embed/live_stream?channel=UCMacEQ-nR-W2QOt1h6oUjsw",
                         "Aljazek" => "https://www.youtube.com/embed/live_stream?channel=UCgkTyjIHIhDoQ5QUtyNzwBA",
